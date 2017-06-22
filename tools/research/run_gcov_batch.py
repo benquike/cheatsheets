8a5b098ca00f5f48efb373d2ff65969c625cb519
Hui Peng <benquike@163.com>
2017-06-22 00:46:35 -0400

    Update tools/research/run_gcov_batch.py

--- tools/research/run_gcov_batch.py
+++ tools/research/run_gcov_batch.py
@@ -4,13 +4,27 @@ import sys
 import os
 import subprocess
 
-def main():
-    if len(sys.argv) != 3:
-        print "Usage: run_gcov_batch.py <bin_path> <dir_to_input>"
-        sys.exit(-1)
+import argparse
 
-    bin_path = os.path.abspath(sys.argv[1])
-    input_dir = os.path.abspath(sys.argv[2])
+def main():
+
+    parser = argparse.ArgumentParser(usage="A tool for running gcov in batch")
+    parser.add_argument("--bin_path", required=True, help="The path to the binary to run")
+    parser.add_argument("--input_dir", required=True, help="The directory containing all the inputs")
+    parser.add_argument("--cov_dir", help="The directory to put the coverage info files")
+
+    args = parser.parse_args()
+
+    bin_path  = os.path.abspath(args.bin_path)
+    # here we assume that the executable binary resides
+    # in the bin directory of the challenge
+    bin_root_dir = os.path.join(os.path.dirname(bin_path), '..')
+    bin_filename = os.path.basename(bin_path)
+    input_dir = os.path.abspath(args.input_dir)
+    cov_dir   = os.path.abspath(args.cov_dir) if args.cov_dir is not None else "lcov-" + bin_filename
+
+    if not os.path.exists(cov_dir):
+        os.makedirs(cov_dir)
 
     for i in os.listdir(input_dir):
         if i == '.' or i == '..':
@@ -21,7 +35,13 @@ def main():
         env["GDB_AUTO_EXEC"] = "1"
         env["GDB_BIN_FILE"]  = bin_path
         env["GDB_INPUT_FILE"] = os.path.join(input_dir, i)
-        subprocess.call(["gdb"], env=env)
+        null_fd = open("/dev/null", "wb")
+        # collect the coverage info
+        subprocess.call(["gdb"], env=env, stdout=null_fd, stderr=null_fd)
+        # generate lcov info
+        subprocess.call(['lcov', '-c', '--directory', bin_root_dir,
+                         '--output-file', os.path.join(cov_dir, i + '.info')],
+                        stdout=null_fd, stderr=null_fd)
 
 if __name__ == '__main__':
     main()
